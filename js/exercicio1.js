b2a77a20c57e8bdb95dfe448be23a64f464834b3
john leal <voltoja35@gmail.com>
2024-10-04 17:12:42 -0300

    exercicio 1 ok

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,11 +1,29 @@
 // 8. Escreva um algoritmo que receba 4 notas. Calcule a média das notas e mostre na tela. Se a média for maior ou igual a 6, escreva "Aprovado", senão escreva "Reprovado".
 
-// Exemplo:
-// 		Entrada:
-// 			nota 1: 7
-// 			nota 2: 6.3
-// 			nota 3: 8
-// 			nota 4: 5.5
+
+   let nota1 = 5, nota2 = 7, nota3 = 8, nota4 = 9, media
+
+   //   calculo das variaveis
+
+     media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+   //   finalizaçao 
+     console.log("media", (media.toFixed(1)));
+
+
+             if (media >= 6){
+               console.log("aprovado");
+
+             if (media > 3 && media < 6) {
+                  console.log("recuperaçao");
+                  
+               } else { console.log("reprovado");
+               
+                  
+               }
+               
+             }
+     
 // 		Saída:
 // 			Média: 6.7
 // 			Aprovado!
